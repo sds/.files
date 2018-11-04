91c37bf4e032c4da56835f258362a0ffbd2dd8ed
Shane da Silva <shane@dasilva.io>
2018-11-04 10:29:58 -0800

    Configure fzf to work with terminal key bindings
    We didn't set this up when we first installed `fzf` since we didn't
    understand the full feature set before diving in.
    
    After a bit of reading, we're ready to activate.
    
    One unfortunate workaround implemented was not using `brew --prefix` to
    dynamically get the location of the shell completion/key-binding files.
    The reason was this was terribly slow to run, and given we source these
    files on every shell, we wanted it to remain performant.

---
+++ plugins/fzf/lib/interactive/fzf.bash
@@ -0,0 +1,4 @@
+# TODO: Come up with a fast alternative to get location dynamically
+# (`brew --prefix fzf` takes too long, so we hardcode this for now)
+source /usr/local/opt/fzf/shell/completion.bash
+source /usr/local/opt/fzf/shell/key-bindings.bash
---
+++ plugins/fzf/lib/interactive/fzf.zsh
@@ -0,0 +1,23 @@
+# Disable activating fzf autocompletion via TAB since in some contexts (e.g.
+# completing a file name in the current directory) it is overkill. Explicit
+# Ctrl-T is our preferred activation mechanism.
+# https://github.com/junegunn/fzf/wiki/Configuring-fuzzy-completion#caveats
+setopt vi
+
+# TODO: Come up with a fast alternative to get location dynamically
+# (`brew --prefix fzf` takes too long, so we hardcode this for now)
+source /usr/local/opt/fzf/shell/completion.zsh
+source /usr/local/opt/fzf/shell/key-bindings.zsh
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
