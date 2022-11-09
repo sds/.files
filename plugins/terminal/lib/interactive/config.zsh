2c076f16b1bb253b8f4905cd84bd5166cd28045f
Shane da Silva <shane@dasilva.io>
2022-11-08 19:30:44 -0800

    Cheat when referencing `gdircolors` zsh config
    For reasons that elude me, the Homebrew `shellenv` script hasn't run by
    this point. Hard code to the Homebrew bin path so we stop getting an
    annoying warning.

--- plugins/terminal/lib/interactive/config.zsh
+++ plugins/terminal/lib/interactive/config.zsh
@@ -13,7 +13,7 @@ zstyle ':completion:*' auto-description 'specify: %d'
 zstyle ':completion:*' completer _expand _complete _correct _approximate
 zstyle ':completion:*' format 'Completing %d'
 zstyle ':completion:*' group-name ''
-zstyle ':completion:*' menu select=2 eval "$(gdircolors -b)"
+zstyle ':completion:*' menu select=2 eval "$(/opt/homebrew/bin/gdircolors -b)"
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
