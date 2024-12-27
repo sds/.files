16d0af996886eaa5d3bf6ce54ed5d24165f7009a
Shane da Silva <shane@dasilva.io>
2024-12-26 17:11:23 -0800

    Minor config adjustments

--- config.fish
+++ config.fish
@@ -1,3 +1,4 @@
+###############################################################################
 # Initialize XDG base directory environment variables as defined in:
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-0.6.html.
 #
@@ -7,32 +8,35 @@
 
 # Directory where user-specific data files should be stored.
 export XDG_DATA_HOME="$HOME/.local/share"
-
 # Preference-ordered set of base directories to search for data files in
 # addition to the $XDG_DATA_HOME base directory.
 export XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
-
 # Directory where user-specific configuration files should be stored.
 export XDG_CONFIG_HOME="$HOME/.config"
-
 # Preference-ordered set of base directories to search for configuration files
 # in addition to the $XDG_CONFIG_HOME base directory.
 export XDG_CONFIG_DIRS="/etc/xdg"
-
 # Directory where user-specific non-essential data files should be stored.
 export XDG_CACHE_HOME="$HOME/.cache"
 
-set -gx PATH /opt/homebrew/bin $PATH
+###############################################################################
+# Update PATH with helpful utilities
+
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/opt/curl/bin # Not installed in default brew bin dir
+
+fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin # Use GNU core utils (`ls` etc) instead of macOS ones
+fish_add_path "$HOME/.files/git-helpers"
+
+###############################################################################
 
 set fish_greeting
 
 alias vim="nvim"
 alias g="git"
 
-# Highlight matches if output to shell
-alias grep="grep --color=auto"
-# Better match highlight color
-set -x GREP_COLOR "0;30;43"
+alias grep="grep --color=auto"  # Highlight matches if output to shell
+set -x GREP_COLOR "0;30;43"     # Better match highlight color
 
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
@@ -44,15 +48,7 @@ set -x LESS "--RAW-CONTROL-CHARS"
 
 set -x EDITOR "nvim"
 
-set -U __done_min_cmd_duration 20000
-
-# Use current terminal to request password for GPG key
-set -x PINENTRY_USER_DATA "USE_CURSES=1"
-# Ensure pinentry program displays to the correct terminal
-set -x GPG_TTY "$(tty)"
-set -x GNUPGHOME "$HOME/.gnupg"
-
-set -x FZF_DEFAULT_COMMAND "fd --hidden --no-ignore --exclude .git --exclude node_modules --type file"
+set -x FZF_DEFAULT_COMMAND "fd --no-ignore --hidden --exclude node_modules"
 
 if which gpgconf >/dev/null 2>&1
   if not pgrep gpg-agent >/dev/null
@@ -60,6 +56,11 @@ if which gpgconf >/dev/null 2>&1
   end
 
   set -x SSH_AUTH_SOCK "$(gpgconf --list-dirs agent-ssh-socket)"
+
+  # Ensure pinentry program displays to the correct terminal
+  set -x GPG_TTY "$(tty)"
+  set -x GNUPGHOME "$HOME/.gnupg"
+  gpg-connect-agent updatestartuptty /bye >/dev/null
 else
   echo "Skipping GPG setup since it is not yet installed."
 end
@@ -76,10 +77,6 @@ function tmux -d "Start tmux session or connect to an existing session based on 
     command tmux $argv
   end
 end
-
-fish_add_path /opt/homebrew/opt/curl/bin
-fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
-fish_add_path "$HOME/.files/git-helpers"
 
 # Load any local configuration, if any
 if test -e "$HOME/.config/fish/local.fish"
